e3d6c34bcd4256706c39d18208ca4651edbfb3a8
roma9811 <makhsoianiroma@gmail.com>
2024-04-14 08:24:58 +0200

    season of the year

--- script.js
+++ script.js
@@ -1,32 +1,59 @@
-  "use strict"
-
-for(let sum = 1; sum <= 5; sum++){
-    console.log(sum);
-}
-let test = 1;
-do{
-    console.log(test);
-    test++;
-}while(test <= 5);
-
-let num = 0;
-while(num < 3){
-    console.log(`Number:${num}`);
-    num++;
-} 
-for(let i =1;i<=5;i++){
-    if(i==3){
-        continue
-    }console.log(i); 
-}
-function checkAge(age){
-    if(age>=18){
-        return true;
+"use strict"
+/*work1*/
+const season1 =(spring) => {
+    if(spring > 10 && spring < 24){
+        return "Spring temperature: " + spring;
     }
+}
+const season2 =(summer) => {
+    if(summer > 20 && summer < 40){
+        return "Summer temperature: " + summer;
     }
-let age=prompt("How old are you?");
-if(checkAge(age)){
-    alert("Welcome")
-}else{
-    alert("You can't access the site")
+}
+const season3 =(autumn) => {
+    if(autumn > 5 && autumn < 16){
+        return "Autumn temperature: " + autumn;
+    }
+}
+const season4 =(winter) => {
+    if(winter > -5 && winter < 10){
+        return "Winter temperature: " + winter;
+    }
+}
+const callSeason=(season,temperature) =>{
+    switch(season){
+        case "spring":
+        return season1(temperature);
+        break;
+        case "summer":
+        return season2(temperature);
+        break;
+        case "autumn":
+        return season3(temperature);
+        break;
+        case "winter":
+        return season4(temperature);
+        break;
+        default:
+        return "Invalid season";
+    }   
+}
+console.log(callSeason("spring",20));
+console.log(callSeason("summer",30));
+console.log(callSeason("autumn",10));
+console.log(callSeason("winter",9));
+
+/*work2*/
+for (let i = -5; i < 40; i++){
+    if(i > 10 && i < 24){
+        console.log("Spring",i);
+    }else if(i > 20 && i < 40){
+        console.log("Summer",i);
+    }else if (i > 5 && i < 16){
+        console.log("Autumn",i);
+    }else if(i > -5 && i < 10){
+        console.log("winter",i);
+    }else{
+        console.log("invalid season");
+    }
 }
